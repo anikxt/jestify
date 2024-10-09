8416678b2fb84dab551ff1aa0201f448ffbcf422
Aniket Kanere <aniket15304@gmail.com>
2024-10-09 14:23:55 +0530

    feat: Ability to search and display memes in cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,68 +1,41 @@
 'use client';
 
-import Image from 'next/image';
-import { ImageKitProvider, IKImage, IKUpload } from 'imagekitio-next';
-import { useState } from 'react';
 import { Button } from '@/components/ui/button';
-
-const publicKey = process.env.NEXT_PUBLIC_PUBLIC_KEY;
-const authenticator = async () => {
-  try {
-    const response = await fetch('http://localhost:3000/api/auth');
-
-    if (!response.ok) {
-      const errorText = await response.text();
-      throw new Error(
-        `Request failed with status ${response.status}: ${errorText}`
-      );
-    }
-
-    const data = await response.json();
-    const { signature, expire, token } = data;
-    return { signature, expire, token };
-  } catch (error) {
-    if (error instanceof Error) {
-      throw new Error(`Authentication request failed: ${error.message}`);
-    } else {
-      throw new Error(
-        `Authentication request failed: An unknown error occurred`
-      );
-    }
-  }
-};
-
-const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
+import { IKImage, IKUpload } from 'imagekitio-next';
+import { useState } from 'react';
+import { urlEndpoint } from './providers';
 
 export default function Home() {
-  const [filePath, setFilePath] = useState('');
+  const [name, setName] = useState<string | null>(null);
 
   return (
     <div className="">
       <Button variant={'destructive'}>Click Me</Button>
-      <ImageKitProvider
-        publicKey={publicKey}
-        urlEndpoint={urlEndpoint}
-        authenticator={authenticator}
-      >
-        {/* ...child components */}
-        {filePath && (
-          <IKImage path={filePath} width={300} height={500} alt="Alt text" />
-        )}
 
-        <div>
-          <h2>File upload</h2>
-          <IKUpload
-            fileName="test-upload.png"
-            onError={(error) => {
-              console.log('error', error);
-            }}
-            onSuccess={(response) => {
-              console.log('success', response);
-              setFilePath(response.filePath);
-            }}
-          />
-        </div>
-      </ImageKitProvider>
+      {name && (
+        <IKImage
+          width={300}
+          height={500}
+          urlEndpoint={urlEndpoint}
+          path={name}
+          transformation={[{ raw: 'l-text,i-hello world,fs-50,l-end' }]}
+          alt="Alt text"
+        />
+      )}
+
+      <div>
+        <h2>File upload</h2>
+        <IKUpload
+          fileName="test-upload.png"
+          onError={(error) => {
+            console.log('error', error);
+          }}
+          onSuccess={(response) => {
+            console.log('success', response);
+            setName(response.name);
+          }}
+        />
+      </div>
     </div>
   );
 }
