f9ef12255243cc03ff65e641cbcb897aeec6d6f8
Aniket Kanere <aniket15304@gmail.com>
2024-10-10 21:45:30 +0530

    feat: Adding the ability to download images

--- src/app/customize/[fileId]/page.tsx
+++ src/app/customize/[fileId]/page.tsx
@@ -13,8 +13,6 @@ export default async function CustomizePage({
   const file = await imagekit.getFileDetails(params.fileId);
   return (
     <div className="container mx-auto space-y-8 py-8 px-4">
-      <h1 className="text-4xl font-bold">Customize</h1>
-
       <CustomizePanel file={{ filePath: file.filePath, name: file.name }} />
     </div>
   );
